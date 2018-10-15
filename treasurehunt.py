e970a78347f9431967977ea7e2f675fc4e5c7f69
Rsattler17 <43052362+Rsattler17@users.noreply.github.com>
2018-10-14 20:41:39 -0500

    Create treasurehunt.py

---
+++ treasurehunt.py
@@ -0,0 +1,10 @@
+#Treasure Hunt Lab
+#Ryan Sattler, Graham Pankratz, Jacob Bussmann
+#10-14-18
+
+#Greeting
+print('Welcome to the Treasure Hunt!')
+
+#User Input/Difficulty level
+input('What difficulty level would you like? (enter: Easy for 3x3, Medium for 4x4, Hard for 5x5)')
+
